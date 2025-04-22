77bb5634d163452c8f3528b085f6279ae8404cf0
Nahom-Derese <nahomderese@gmail.com>
2025-04-22 15:33:51 +0300

    Lec 1 - Smart Contract

---
+++ C1 Solidity Introduction/L1 Smart Contract/Data_type/Unsigned_Integers.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Contract {
+    uint8 public a = 8;
+    uint16 public b = 455;
+    uint256 public sum = a + b;
+}
---
+++ C1 Solidity Introduction/L1 Smart Contract/Data_type/string_Literals.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Contract {
+    bytes32 public msg1 = "Hello World";
+    string public msg2 =
+        "Create a public string storage variable msg2 which stores a string literal that requires over 32 bytes to store.";
+}
